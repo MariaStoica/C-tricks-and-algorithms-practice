ce50d12b1e63f118837aacc63faf0d751c555d54
Maria Stoica <mstoic@microsoft.com>
2016-05-14 14:23:36 -0700

    lambda dictionary file names and bytes

--- CSharpTricks/CSharpTricks/Program.cs
+++ CSharpTricks/CSharpTricks/Program.cs
@@ -10,6 +10,14 @@ namespace CSharpTricks
     {
         static void Main(string[] args)
         {
+            // The classes in this poject each showcase a concept from C#.
+            // Each class is responsible for its own output and the spaces between the classes' output is handled by Main.
+
+            LambdaExpressions.LambdaExpressionsTutorial();
+            Console.WriteLine("\n\n");
+
+            // After all is said and done.
+            Console.ReadLine();
         }
     }
 }
